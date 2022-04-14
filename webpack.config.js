2485050f7be26c978b4eb6bcc91a84becb1754b5
Julio <juliolh06@gmail.com>
2022-04-13 22:24:05 -0500

    First Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
-require('dotenv').config();
-// const Dotenv = require('dotenv-webpack');
 
-/** @type {import('webpack').Configuration} */
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath:'/'
+    publicPath: '/',
   },
   mode: 'development',
   resolve: {
@@ -36,13 +31,12 @@ module.exports = {
         ],
       },
       {
-        test: /\.(s*)css$/,
+        test: /\.css$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           'css-loader',
-          'sass-loader',
         ],
       },
     ],
@@ -55,22 +49,13 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
     }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'public/manifest.json', to: '' },
-        { from: 'public/service-worker.js', to: '' },
-        { from: 'public/icon.png', to: 'assets' },
-      ],
-    }),
-    new webpack.DefinePlugin({
-      'process.env.PAYPAL_CLIENT_PP': JSON.stringify(process.env.PAYPAL_CLIENT_PP),
-      'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.GOOGLE_MAPS_API_KEY),
-		}),
   ],
   devServer: {
-    allowedHosts: path.join(__dirname, 'dist'),
-    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     compress: true,
     port: 3005,
+    historyApiFallback: true,
   },
 };
