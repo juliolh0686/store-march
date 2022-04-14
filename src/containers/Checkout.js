6866169aca1c3b458f5e652536af3817ad2a2434
Julio <juliolh06@gmail.com>
2022-04-13 22:54:34 -0500

    [add] reat-helmet

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import { Helmet } from 'react-helmet';
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
@@ -18,30 +19,35 @@ const Checkout = () => {
   }
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        <h3> {cart.length > 0 ? 'Lista de Pedidos':'Sin Pedidos'} </h3>
-        {cart.map((item, index) => (
-          <div className='Checkout-item' key={index}>
-            <div className='Checkout-element'>
-              <h4>{item.title}</h4>
-              <span>$ {item.price} </span>
+    <>
+      <Helmet>
+        <title>Lista de Pedidos - Store Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          <h3> {cart.length > 0 ? 'Lista de Pedidos':'Sin Pedidos'} </h3>
+          {cart.map((item, index) => (
+            <div className='Checkout-item' key={index}>
+              <div className='Checkout-element'>
+                <h4>{item.title}</h4>
+                <span>$ {item.price} </span>
+              </div>
+              <button type='button' onClick={handleRemove(item)}>
+                <i className="fa-solid fa-trash-can"></i>
+              </button>
             </div>
-            <button type='button' onClick={handleRemove(item)}>
-              <i className="fa-solid fa-trash-can"></i>
+          ))}
+        </div>
+        {cart.length > 0 && (
+          <div className='Checkout-sidebar'>
+            <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+            <button type='button'>
+              <Link to='/checkout/information'>Continuar Pedido</Link>
             </button>
           </div>
-        ))}
+        )}
       </div>
-      {cart.length > 0 && (
-        <div className='Checkout-sidebar'>
-          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-          <button type='button'>
-            <Link to='/checkout/information'>Continuar Pedido</Link>
-          </button>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Products from '../components/Products';
+import { Helmet } from 'react-helmet';
 import initialState from '../initialState';
 
 const Home = () => {
   return (
+    <>
+    <Helmet>
+      <title> Store Merch - Productos</title>
+    </Helmet>
       <Products products={initialState.products} />
+    </>
   );
 }
 
